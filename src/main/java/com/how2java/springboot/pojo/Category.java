bb1e90dbaed639368b9b2717670baaaade9b2ab0
陈陈陈骐 <334972898@qq.com>
2018-08-09 14:03:40 +0800

    springboot与thymeleaf的简单应用

---
+++ src/main/java/com/how2java/springboot/pojo/Category.java
@@ -0,0 +1,23 @@
+package com.how2java.springboot.pojo;
+
+/**
+ * @Author sya
+ * @Date 2018/8/8
+ */
+public class Category {
+    private int id;
+
+    private String name;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
